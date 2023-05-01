f535675c3d6d4e4362360d87a637f25a72dabcd8
Bhuvan Chand <bhuvanchand.dasari.offcial@gmail.com>
2023-05-01 06:41:59 +0530

    fourth commit

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ import pickle
 #from sklearn.preprocessing import LabelEncoder
 import math
 app = Flask(__name__)
+#localhost
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+#internal
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pdetect_user:AVB0AgbRcRl020rbg1Lfn8rkiC28WhOK@dpg-ch7g77o2qv26p1cuu9j0-a/pdetect'
+
 #external
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pdetect_user:AVB0AgbRcRl020rbg1Lfn8rkiC28WhOK@dpg-ch7g77o2qv26p1cuu9j0-a.oregon-postgres.render.com/pdetect'
 
@@ -26,10 +30,10 @@ class UserPassword(db.Model):
     patient_type_ = db.Column(db.String(50))
     
 
-    def __init__(self, email_, password_, patient_password_):
+    def __init__(self, email_, password_, patient_type_):
         self.email_ = email_
         self.password_ = password_
-        self.patient_type_ = patient_password_
+        self.patient_type_ = patient_type_
 
 @app.route('/')
 def index():
@@ -42,9 +46,9 @@ def login():
 
     email = content["email"]
     password = content["password"]
-    
+    print(email)
 
-    user = UserPassword.query.filter_by(email).first()
+    user = UserPassword.query.filter_by(email_= email).first()
     if user is None:
         return jsonify({"user":0})
     
@@ -61,20 +65,23 @@ def createaccount():
 
     email = content["email"]
     password = content["password"]
-    confirm_password = content["password"]
+    confirm_password = content["confirm_password"]
     patient_type =  content["patient_type"]
 
 
-    user = UserPassword.query.filter_by(email).first()
+    user = UserPassword.query.filter_by(email_ = email).first()
     if user is None:
-        add_request = UserPassword(email_ = email, password_ = password, patient_type_ = patient_type)
+        
         
         if password == confirm_password:
+            add_request = UserPassword(email_ = email, password_ = password, patient_type_ = patient_type)
             db.session.add(add_request)
             db.session.commit()
+
+            return jsonify({"user":1})
         
         else:
-            return jsonify({"user":0})
+            return jsonify({"user":2})
     else:
         return jsonify({"user":0})
 
